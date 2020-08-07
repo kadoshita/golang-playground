0b77a69ae35b7cef8fedc729e514a810d58dc850
kadoshita <sublimer.me@gmail.com>
2020-08-07 22:11:13 +0900

    forとif

---
+++ flowcontrol/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	return lim
+}
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+
+	return fmt.Sprint(math.Sqrt(x))
+}
+func main() {
+	fmt.Println(pow(3, 2, 10), pow(3, 3, 20))
+}
