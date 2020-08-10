494d1a5812471b0032a29354ed20b6b78593a53e
kadoshita <sublimer.me@gmail.com>
2020-08-10 13:09:21 +0900

    Function closures

--- moretypes/main.go
+++ moretypes/main.go
@@ -182,6 +182,11 @@ func main() {
 
 	fmt.Println(compute(hypot))
 	fmt.Println(compute(math.Pow))
+
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(pos(i), neg(-2*i))
+	}
 }
 
 func printSlice(s []int) {
@@ -198,4 +203,12 @@ func WordCount(s string) map[string]int {
 
 func compute(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
+}
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
