1d1a2fe4a16dc98fc10509d97957d1661f3aa076
kadoshita <sublimer.me@gmail.com>
2020-08-10 10:34:44 +0900

    switchとdefer

--- flowcontrol/main.go
+++ flowcontrol/main.go
@@ -2,24 +2,53 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
+	"time"
 )
 
-func pow(x, n, lim float64) float64 {
-	if v := math.Pow(x, n); v < lim {
-		return v
-	} else {
-		fmt.Printf("%g >= %g\n", v, lim)
-	}
-	return lim
-}
-func sqrt(x float64) string {
-	if x < 0 {
-		return sqrt(-x) + "i"
+func main() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("$s.\n", os)
 	}
 
-	return fmt.Sprint(math.Sqrt(x))
-}
-func main() {
-	fmt.Println(pow(3, 2, 10), pow(3, 3, 20))
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In to days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+
+	defer fmt.Println("world")
+
+	fmt.Println("hello")
+
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
 }
